9262a65bc8d9f9a09dce8a1e7d64a61ff9dd0e2b
Sreemanta Barman <100501976+clenser@users.noreply.github.com>
2024-06-04 20:38:59 +0530

    Add files via upload

---
+++ mpu6050/mpu6050_servo.py
@@ -0,0 +1,31 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <Servo.h>
+
+Servo servo;
+Adafruit_MPU6050 srituhobby;
+
+void setup(void) {
+  Serial.begin(115200);
+  servo.attach(3);
+  Wire.begin();
+  srituhobby.begin();
+  servo.write(90); // Center the servo initially
+
+  srituhobby.setAccelerometerRange(MPU6050_RANGE_8_G); // 2_G, 4_G, 8_G, 16_G
+  srituhobby.setGyroRange(MPU6050_RANGE_500_DEG);      // 250, 500, 1000, 2000
+  srituhobby.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  delay(100);
+}
+
+void loop() {
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  srituhobby.getEvent(&a, &g, &temp);
+
+  int value = map(a.acceleration.x, -10, 10, 0, 180); // Map accelerometer value directly to servo position
+  servo.write(value);
+  Serial.println(value);
+}
